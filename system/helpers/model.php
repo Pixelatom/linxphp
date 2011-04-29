e3aafd6e5ef9565b3dc0063a59964103b5e9ad5e
Javier Arias <javis.arias@gmail.com>
2011-04-29 19:15:24 +0000

    added some code for getters and setters

--- system/helpers/model.php
+++ system/helpers/model.php
@@ -1,5 +1,86 @@
 <?php
 abstract class Model{
+    
+    function  __construct() {
+        $class_name = get_class($this);
+        $function = new ReflectionClass($class_name);
+        $properties = $function->getDefaultProperties();
+
+        // removes variables when there are getters and setters in the format __get_variable
+        foreach ($properties as $name=>$value){
+            
+            $methods_name = array('__get_'.$name,'__set_'.$name);
+            foreach($methods_name as $method_name){
+                
+                if (method_exists($this, $method_name)){
+
+                    unset($this->$name);
+                    //die('accessible false');
+                    //$prop = $function->getProperty($name);
+                    //$prop->setAccessible(false); // we are making this property private here.. I think
+                    
+                }
+            }
+        }
+    }
+    /*
+    function  __set($name, $value) {
+        // we'll check if the unset variable is part of the model
+        $class_name = get_class($this);
+        $function = new ReflectionClass($class_name);
+        $properties = $function->getDefaultProperties();
+
+        if (array_key_exists($name,$properties)){
+            $reflection = new ReflectionProperty($class_name, $name);
+            if ($reflection->isPublic() ){
+                // is part of the model!!
+
+                // we check if there is a getter existing
+                $methodname = '__set_'.$name;
+                if (method_exists($this, $methodname)){
+                    return $this->$methodname($value);
+                }
+
+                // set the non accesible internal property
+                $this->$name = $value;
+            }
+        }
+    }
+     * 
+     */
+    function  __get($name) {
+        // when checking for unset variables
+        // we'll check if the unset variable is part of the model
+        $class_name = get_class($this);
+        $function = new ReflectionClass($class_name);
+        $properties = $function->getDefaultProperties();
+
+        if (array_key_exists($name,$properties)){
+            $reflection = new ReflectionProperty($class_name, $name);
+            if ($reflection->isPublic() ){
+                // is part of the model!!
+
+                // we check if there is a getter existing
+                $methodname = '__get_'.$name;
+                //if (method_exists($this, $methodname)){
+                //    return $this->$methodname();
+                //}
+
+                // then we try to return a lazy_load property
+                Mapper::_load_relationship($this,$name);
+                return $this->$name;
+            }
+        }
+
+        $trace = debug_backtrace();
+        trigger_error(
+        'Undefined property '.$class_name.'::' . $name .
+        ' in ' . $trace[0]['file'] .
+        ' on line ' . $trace[0]['line'],
+        E_USER_NOTICE);
+        return null;
+
+    }
     // protected methods accesible only for 'friends' classes
     protected function _before_insert(){
     }
@@ -54,29 +135,5 @@ abstract class Model{
             ' on line ' . $trace[0]['line'],
             E_USER_NOTICE);
     }
-    function  __get($name) {
-       
-        $class_name = get_class($this);
-        $function = new ReflectionClass($class_name);
-        $properties = $function->getDefaultProperties();
-
-        
-        
-        if (array_key_exists($name,$properties)){
-            $reflection = new ReflectionProperty($class_name, $name);
-            if ($reflection->isPublic() ){
-                Mapper::_load_relationship($this,$name);
-                return $this->$name;
-            }
-        }
-        
-        $trace = debug_backtrace();
-        trigger_error(
-        'Undefined property '.$class_name.'::' . $name .
-        ' in ' . $trace[0]['file'] .
-        ' on line ' . $trace[0]['line'],
-        E_USER_NOTICE);
-        return null;
-                
-    }
+    
 }
