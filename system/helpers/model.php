aae9b00e31ca265629ef73915c8c8a199ec7744b
Javier Arias <javis.arias@gmail.com>
2011-04-29 20:30:21 +0000

    no more "friend class" emulation. now model methods are public

--- system/helpers/model.php
+++ system/helpers/model.php
@@ -82,58 +82,20 @@ abstract class Model{
 
     }
     // protected methods accesible only for 'friends' classes
-    protected function _before_insert(){
+    public function _before_insert(){
     }
-    protected function _before_update(){
+    public function _before_update(){
     }
-    protected function _before_delete(){
+    public function _before_delete(){
     }
-    protected function _after_insert(){
+    public function _after_insert(){
     }
-    protected function _after_update(){
+    public function _after_update(){
     }
-    protected function _after_delete(){
+    public function _after_delete(){
     }
-    protected function _after_load(){
+    public function _after_load(){
     }
-    // simulation of a Friend Class methods
-    public function __call($name, $arguments) {
-
-        $trace = debug_backtrace();
-
-        
-        
-        if(isset($trace[2]['class']) && ($trace[2]['class'] == 'SQLMapperDriver' or in_array('SQLMapperDriver', class_parents($trace[2]['class'])))) {
-            switch ($name){
-                case '_before_insert':
-                    return $this->_before_insert($arguments);
-                    break;
-                case '_before_update':
-                    return $this->_before_update($arguments);
-                    break;
-                case '_before_delete':
-                    return $this->_before_delete($arguments);
-                    break;
-                case '_after_insert':
-                    return $this->_after_insert($arguments);
-                    break;
-                case '_after_update':
-                    return $this->_after_update($arguments);
-                    break;
-                case '_after_delete':
-                    return $this->_after_delete($arguments);
-                    break;
-                case '_after_load':
-                    return $this->_after_load($arguments);
-                    break;
-            }
-        }
-        $class_name = get_class($this);
-        trigger_error(
-            'Undefined method '.$class_name.'::' . $name .
-            ' in ' . $trace[0]['file'] .
-            ' on line ' . $trace[0]['line'],
-            E_USER_NOTICE);
-    }
+    
     
 }
