7996b94e158ca45bfe53bc03bf6f784f2745cddc
Javier Arias <javis.arias@gmail.com>
2011-11-02 16:03:26 -0300

    added support for 'multiple' attribute on select widgets

--- modules/form/classes/selectwidget.php
+++ modules/form/classes/selectwidget.php
@@ -9,36 +9,56 @@ class SelectWidget extends FormWidget {
 
     public $options;
     public $selected;
-    public $null;
+    
 
     public function __construct($name, $properties) {
+        
+
         parent::__construct($name, $properties);
+        
+        
+
+        //$this->value = $this->get_value();
 
         $this->set_default_template('form/widgets/select');
 
-	    extract($properties, EXTR_SKIP);
-
-        // pasar al template
-        $selected = array();
-        foreach ($options as $value => $title) {
-            if (isset($_POST[$name])):
-                $selected[$value] = ( $this->get_value() == $value ) ? 'selected="selected"' : '';
-            else:
-                $selected[$value] = ($default == $value ) ? 'selected="selected"' : '';
-            endif;
-        }
-    	if(isset($null) and $null == 'true')
-    	    $this->null = true;
+	    
         
-        $this->options = $options;
-        $this->selected = $selected;
+        $this->options = (isset($this->properties['options']))?$this->properties['options']:array();
+        
     }
 
     public function show($name = null) {
+        
+        
+        $options = $this->options;
+        // pasar al template
+        $selected = array();
+        
+        foreach ($options as $value => $title) {
+            if (!is_array($this->get_value())){
+
+                $selected[$value] = ( $this->get_value() == $value ) ? 'selected="selected"' : '';
+            }
+            elseif(is_array($this->get_value())){
+                /*
+                echo '<pre>';
+                var_dump($this->get_value());
+                die();
+                */
+                $selected[$value] = ( in_array($value,$this->get_value()) ) ? 'selected="selected"' : '';    
+            }
+            
+        }
+        
+        $this->selected = $selected;
+
         $this->set('selected', $this->selected);
         $this->set('options', $this->options);
-        $this->set('null', $this->null);
+        if (isset($this->properties['attributes']['multiple']))
+            $this->name .= "[]";
         parent::show($name);
+        $this->name = str_replace("[]", "", $this->name);
     }
 
 }
--- modules/form/templates/form/widgets/select.php
+++ modules/form/templates/form/widgets/select.php
@@ -2,9 +2,9 @@
     <label for="<?=$id?>"><?=$label?></label>
     <div class="input">
         <select <?foreach ($attributes as $property_name=>$property_value): echo " {$property_name}='{$property_value}' "; endforeach;?> name="<?=$name?>" id="<?=$id?>" autocomplete="off">
-            <?=($null)?'<option value="">Please Select</option>':'';?>
+            
             <?foreach ( $options as $value => $title ):?>
-            <option value='<?=$value?>' <?=$selected[$value]?> ><?=$title?></option>
+            <option value='<?=$value?>' <?=(isset($selected[$value]))?$selected[$value]:''?> ><?=$title?></option>
             <?endforeach;?>
         </select>
         <?if (isset($error)):?>
