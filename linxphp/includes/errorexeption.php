732462f8c4d8ca0cd6c2a708ae0abb54dc76d1e4
Javier Arias <javis.arias@gmail.com>
2011-11-08 16:20:09 -0300

    ignores new DEPRECATED error to allow developers to use third party libraries

--- linxphp/includes/errorexeption.php
+++ linxphp/includes/errorexeption.php
@@ -10,6 +10,8 @@ class FatalErrorException extends ErrorException {}
 
 function exceptionsHandler($code, $string, $file, $line) {
 	switch ($code){
+		case E_DEPRECATED: // ignores new DEPRECATED error to allow developers to use third party libraries
+   			return;
 		case E_WARNING:
 			throw new WarningException($string, $code, $code,$file,$line);
 		default:
