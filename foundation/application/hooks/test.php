ee39f243d42ffae39ed99d2071a634772452a451
Javier Arias <javis@javis-GA-A55M-DS2.(none)>
2012-09-29 20:17:03 -0300

    routing: first steps

--- foundation/application/hooks/test.php
+++ foundation/application/hooks/test.php
@@ -2,10 +2,6 @@
 use linxphp\common\Event;
 
 Event::add('system.ready',function(){
-    //echo '<p>System Ready</p>';
-    $request = linxphp\rest\Request::fromRoute('lol');
-    
-    echo $request->url().'<br>';
-    echo linxphp\common\URL::current();
+    //echo '<p>System Ready</p>';    
 });
 
--- src/linxphp/rest/Router.php
+++ src/linxphp/rest/Router.php
@@ -8,12 +8,12 @@ class Router {
     * @var array
     */
     protected static $routes = array(
-            'GET'    => array(),
-            'POST'   => array(),
-            'PUT'    => array(),
-            'DELETE' => array(),
-            'PATCH'  => array(),
-            'HEAD'   => array(),
+        'GET'    => array(),
+        'POST'   => array(),
+        'PUT'    => array(),
+        'DELETE' => array(),
+        'PATCH'  => array(),
+        'HEAD'   => array(),
     );
    
     /**
@@ -30,13 +30,30 @@ class Router {
      * @param callable $callback
      * @throws \Exception 
      */
-    public static function register($method,$route,$callback){
+    public static function register($method,$route,callable $callback){
         // validates method
         $method = strtoupper($method);        
-        if (!in_array($method,self::$methods)) throw new \Exception("Method invalid or not supported to be registered");
+        if (!in_array($method,self::$methods)) throw new \Exception("Method {$method} invalid or not supported to be registered");
         
+        self::$routes[$method][$route] = $callback;
+    }
+    
+    
+    public static function route(Request $request){
         
+        if (!in_array($request->method,self::$methods)) throw new \Exception("Method {$request->method} invalid or not supported by the router");
         
-        self::$routes[$method] = $route;
-    } 
+        foreach(self::$routes[$request->method] as $route=>$handler){
+            $pattern = '#^'.$route.'$#';
+
+            // If we get a match we'll return the route and slice off the first
+            // parameter match, as preg_match sets the first array item to the
+            // full-text match of the pattern.
+            if (preg_match($pattern, $request->route, $parameters))
+            {
+                $parameters = array_slice($parameters, 1);
+                return call_user_func_array($handler, $parameters);
+            }
+        }
+    }
 }
