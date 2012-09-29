a424eedb1a3acf6f7f68de9418a24751e60824f7
Javier Arias <javis@javis-GA-A55M-DS2.(none)>
2012-09-29 20:28:28 -0300

    routing: first steps

---
+++ examples/routing/index.php
@@ -0,0 +1,15 @@
+<?php
+// includes ClassLoader implementation from the framework
+require_once '../../src/linxphp/common/ClassLoader.php';
+
+// Autoloading configuration. Path needs to point to linxphp src folder
+$classLoader = new linxphp\common\ClassLoader('linxphp', "../../src");
+$classLoader->register();
+
+use linxphp\rest\Router;
+
+Router::register('GET', '/lol/lala', function(){
+    echo ':D';
+});
+    
+Router::route(new linxphp\rest\Request());
