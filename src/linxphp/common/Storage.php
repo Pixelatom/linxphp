8fac71ce4135c4725a3f409b18260a2f8e8c3ebe
Javier Arias <javier.arias@pixelatom.com>
2012-12-19 14:50:40 -0300

    new Storage object

--- src/linxphp/common/Storage.php
+++ src/linxphp/common/Storage.php
@@ -9,8 +9,36 @@ class Storage implements Countable , Iterator , Traversable ,  ArrayAccess {
     protected $position = 0;
     protected $array = array();
     
-    public function __construct() {
-        $this->position = 0;
+    // properties setters and getters
+    public function __set($name, $value)
+    {   
+        $this->array[$name] = $value;
+    }
+
+    public function &__get($name)
+    {
+        if (array_key_exists($name, $this->array)) {
+            return $this->array[$name];
+        }
+
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined property via __get(): ' . $name .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE);
+     
+        return null;
+    }
+    
+    public function __isset($name)
+    {
+        return isset($this->array[$name]);
+    }
+
+    public function __unset($name)
+    {
+        unset($this->array[$name]);
     }
     
     // countable methods
